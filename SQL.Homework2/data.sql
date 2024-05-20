5b1bd9a11a68098c9e28233496bcd0f7d3f452a1
ShVIGIR <shvigir@gmail.com>
2024-05-20 21:35:42 +0200

    Homework - class2 - added

---
+++ SQL.Homework2/data.sql
@@ -0,0 +1,38 @@
+﻿-- Requirements:
+
+--● Find all Students with FirstName = Antonio
+SELECT * FROM Student
+WHERE [FirstName] = 'Antonio'
+GO
+
+--● Find all Students with DateOfBirth greater than ‘01.01.1999’
+SELECT * FROM Student 
+WHERE [DateOfBirth] > '01.01.1999'
+GO
+
+--● Find all Students with LastName starting With ‘J’ enrolled in January/1998
+SELECT * FROM Student 
+WHERE [LastName] LIKE 'J%' AND [EnrolledDate] >= '01.01.1998' AND [EnrolledDate] <= '1998.01.31'
+GO
+
+--● List all Students ordered by FirstName
+SELECT * FROM Student
+ORDER BY FirstName ASC
+GO
+
+--● List all Teacher Last Names and Student Last Names in single result set. Remove duplicates
+SELECT LastName FROM Teacher
+UNION ALL
+SELECT LastName FROM Student
+GO
+
+--no duplicates
+SELECT LastName FROM Teacher
+UNION 
+SELECT LastName FROM Student
+GO
+
+
+--● Create Foreign key constraints from diagram or with script
+--● List all possible combinations of Courses names and AchievementType names that can be passed by student
+--● List all Teachers without exam Grade
